22ee8543e5a4b03d029f8323abc1f336d1311fc9
Your Name <you@example.com>
2022-04-18 22:04:39 +0600

    inputs

--- src/app-info.js
+++ src/app-info.js
@@ -1,3 +1,4 @@
 export default {
-  title: "App Name"
+  title: "App Name",
+  
 };
--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
-// import "@/assets/styles/reset.scss";
-
-// import "devextreme/dist/css/dx.common.css";
-
-// import "./themes/generated/theme.base.css";
-// import "./themes/generated/theme.additional.css";
-
 import "@/assets/styles/global.scss";
 import "@/assets/styles/dx-custom-styles.scss";
-
-// import "devextreme/dist/css/dx.greenmist.css";
-
+// ---------------------------------------------
 import "devextreme/dist/css/dx.material.teal.light.css";
-
+// ---------------------------------------------
+import config from "devextreme/core/config";
+config({
+  editorStylingMode: "outlined"
+});
+// ---------------------------------------------
 import ruMessages from "devextreme/localization/messages/ru.json";
 import { locale, loadMessages } from "devextreme/localization";
 loadMessages(ruMessages);
 locale(navigator.language);
+// ---------------------------------------------
 
 import { createApp } from "vue";
 import router from "./router/router";
